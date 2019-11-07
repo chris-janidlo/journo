aabeb6128a7e87725c519a6f2013de5421a3d085
crassSandwich <christopherjanidlo@gmail.com>
2019-11-06 20:17:27 -0500

    Bottom bar in a good place

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import {
+  Box,
+  Paper,
+  InputBase,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 import storyContent from './story.json';
 import { Story } from 'inkjs';
 
-export default function App() {
+const useStyles = makeStyles(theme => {
+  const margin = theme.spacing(2);
+  const widthMinusMargin = `calc(100vw - ${margin * 2}px)`;
+
+  return {
+    app: {
+      height: '100vh'
+    },
+    bottomBar: {
+      width: '100vw',
+      position: 'fixed',
+      bottom: 0
+    },
+    mainInput: {
+      backgroundColor: theme.palette.primary.main,
+      width: widthMinusMargin,
+      margin: 'auto', // left and right
+      marginBottom: margin,
+      marginTop: margin
+    }
+  }
+});
+
+function BottomBar () {
+  const classes = useStyles();
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          {new Story(storyContent).ContinueMaximally()}
-        </Typography>
-      </Box>
-    </Container>
+    <Paper elevation={4} square className={classes.bottomBar}>
+      <Paper elevation={0} className={classes.mainInput}>
+        <Box m={1.2}>
+          <InputBase fullWidth placeholder='Say something...'/>
+        </Box>
+      </Paper>
+    </Paper>
+  );
+}
+
+export default function App () {
+  const classes = useStyles();
+  return (
+    <Box className={classes.app}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {new Story(storyContent).ContinueMaximally()}
+      </Typography>
+      <BottomBar />
+    </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#ccc',
     },
     secondary: {
       main: '#19857b',
