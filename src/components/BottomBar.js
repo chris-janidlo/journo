3c72f85f5e0e6e716a4c21482cc20b562f238fc8
crassSandwich <christopherjanidlo@gmail.com>
2019-11-09 18:00:35 -0500

    Input prompts based on ink that display typing mistakes

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import {
-	Box,
 	Paper,
-	InputBase,
+	TextField,
+	List,
+	ListItem,
+	Typography,
+	Divider,
 	makeStyles
 } from '@material-ui/core';
 
@@ -14,25 +17,184 @@ const useStyles = makeStyles(theme => {
 			position: 'fixed',
 			bottom: 0
 		},
-		mainInput: {
-			backgroundColor: theme.palette.secondary.main,
-			marginLeft: margin,
-			marginRight: margin,
-			marginTop: margin,
-			marginBottom: margin
+		input: {
+			width: `calc(100vw - ${2 * margin}px)`,
+			margin: margin,
+			borderRadius: theme.shape.borderRadius,
+			backgroundColor: '#f2f2f2',
 		}
 	}
 });
 
+function arrayStartsWith (first, second) {
+	for (let i = 0; i < second.length; i++) {
+		if (first[i] !== second[i]) return false;
+	}
+	return true;
+}
+
+// takes text and color
+// text can be either a string or array of strings
+// color is the Material UI Typography color attribute
+function ColoredText (props) {
+	let text = '';
+
+	if (props.text.isArray) {
+		text = props.text.join('');
+	}
+	else {
+		text = props.text;
+	}
+
+	return (
+		<Typography
+			component='span'
+			color={props.color}
+		>
+			{text}
+		</Typography>
+	)
+}
+
+function Prompt (props) {
+	const classes = useStyles();
+
+	const inputSymbols = props.inputSymbols; // player input
+	const targetSymbols = props.targetSymbols; // text for this prompt
+
+	const n = props.longestStartsWithLength;
+
+	let textElements = [];
+	let sharedLength = 0;
+	let greyed = false;
+
+	if (inputSymbols.length === 0) {
+		greyed = true;
+	}
+	else {
+		for (let i = 0; i < targetSymbols.length; i++) {
+			if (i < inputSymbols.length && arrayStartsWith(targetSymbols, inputSymbols.slice(0, i + 1))) {
+				sharedLength++;
+				textElements.push(<ColoredText text={inputSymbols[i]} color='textPrimary' />);
+			}
+			else {
+				if (sharedLength !== n) {
+					if (sharedLength > n) {
+						throw new Error('target should not share more characters with the input than we\'ve already calculated as the max');
+					}
+					// another prompt starts with more characters from the input, so we can assume the player isn't trying to type this prompt out
+					greyed = true;
+					break;
+				}
+				else if (inputSymbols.length > i) {
+					// this prompt has the most shared characters with the input, but the input has some amount of additional characters that aren't shared; those must be typos
+					textElements.push(<ColoredText text={inputSymbols[i]} color='error' />);
+				}
+				else {
+					// this prompt has the most shared characters with the input, and we've exhausted the input but still have more target characters. color those in grey since we still need to type them
+					textElements.push(<ColoredText text={targetSymbols[i]} color='textSecondary' />);
+				}
+			}
+		}
+		// handle any extra input characters
+		for (let i = targetSymbols.length; i < inputSymbols.length; i++) {
+			textElements.push(<ColoredText text={inputSymbols[i]} color='error' />);
+		}
+	}
+
+	return (
+		<ListItem>
+			<Typography>
+				{
+					greyed
+						? <ColoredText text={targetSymbols} color='textSecondary' />
+						: textElements
+				}
+			</Typography>
+		</ListItem>
+	);
+}
+
+// TODO: change to only highligh incorrect characters? ie if the prompt is 'the cat' and the input is 'teh cat' only highlight 'eh' and not 'cat'
+// prompt highlighting algorithm:
+	// if inputLength is 0: color every prompt in grey.
+	// else:
+		// determine which prompt(s) starts with the most characters from the input - call that number n.
+		// for the prompt(s) that share the first n characters with the input:
+			// color the first n characters in black.
+			// if the input is exactly n characters: color the rest of the characters grey.
+			// else: color the next (inputLength - n) characters red.
+		// for the prompt(s) that do not share the first n characters, color every character grey.
+
+function Prompts (props) {
+	const classes = useStyles();
+
+	const choices = props.choices;
+	if (!Array.isArray(choices) || !choices.length) return null;
+
+	const choiceTexts = choices.map(c => c.text);
+
+	const firstPrompt = choiceTexts[0];
+	const prompts = choiceTexts.slice(1);
+
+	const inputSymbols = [...props.inputText]; // in case input contains unicode (https://stackoverflow.com/q/46157867/5931898)
+
+	let longestStartsWithLength = 0;
+
+	if (inputSymbols.length !== 0) {
+		for (let i = 0; i < inputSymbols.length; i++) {
+			if (choiceTexts.some(p => arrayStartsWith([...p], inputSymbols.slice(0, i + 1)))) {
+				longestStartsWithLength = i + 1;
+			}
+		}
+	}
+
+	return (
+		<Fragment>
+			<List>
+				<Prompt
+					longestStartsWithLength={longestStartsWithLength}
+					inputSymbols={inputSymbols}
+					targetSymbols={[...firstPrompt]}
+				/>
+				{prompts.map(p =>
+					<Fragment>
+						<Divider />
+						<Prompt
+							longestStartsWithLength={longestStartsWithLength}
+							inputSymbols={inputSymbols}
+							targetSymbols={[...p]}
+						/>
+					</Fragment>
+				)}
+			</List>
+			<Divider />
+		</Fragment>
+	);
+}
+
+function Input (props) {
+	const classes = useStyles();
+	return (
+		<TextField
+			className={classes.input}
+			variant='outlined'
+			margin='dense'
+			placeholder='Say something...'
+			onChange={o => props.onChange(o.target.value)}
+		/>
+	);
+}
+
 export function BottomBar (props) {
-  const classes = useStyles();
+	const classes = useStyles();
+	
+	const [inputText, setInputText] = useState('');
+
   return (
-    <Paper elevation={4} square className={classes.bottomBar}>
-      <Paper elevation={0} className={classes.mainInput}>
-        <Box m={1.2}>
-          <InputBase fullWidth placeholder='Say something...'/>
-        </Box>
-      </Paper>
+    <Paper className={classes.bottomBar} elevation={4} square>
+			<Prompts choices={props.story.currentChoices} inputText={inputText}/>
+			<Input onChange={setInputText}/>
     </Paper>
   );
 }
