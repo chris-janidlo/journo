b968f19236ffd73d5996df0104203898014d6d78
crassSandwich <christopherjanidlo@gmail.com>
2019-12-17 16:50:19 -0500

    Add lock and stuff for funsies

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,22 +1,59 @@
 import React, { Fragment } from 'react';
 import {
+  makeStyles,
   AppBar,
   Toolbar,
-  Typography
+  Typography,
+  IconButton,
+  Tooltip
 } from '@material-ui/core';
+import {
+  Menu as MenuIcon,
+  Lock as LockIcon
+} from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => {
+  return {
+    menuButton: {
+      marginRight: theme.spacing(2)
+    }
+  };
+});
+
+function LockDisplay (props) {
+  return (
+    <Fragment>
+      <span>&ensp;</span>
+      <Tooltip title="This session is secured with end-to-end encryption.">
+        <LockIcon fontSize='small' />
+      </Tooltip>
+    </Fragment>
+  );
+}
 
 export function TopBar (props) {
+  const classes = useStyles();
+
   const { chatPartner } = props;
 
-  const title = (chatPartner === '') ? 'Inbox' : `Connected with ${chatPartner}`;
+  const partnerConnected = chatPartner !== '';
+  const title = partnerConnected ? `Connected with ${chatPartner}` : 'Inbox';
 
   return (
     <Fragment>
       <AppBar position="fixed" elevation={0}>
         <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+          >
+            <MenuIcon /> {/* TODO: menu with accessibility mode here */}
+          </IconButton>
           <Typography variant="h6" noWrap>
             {title}
           </Typography>
+          {partnerConnected ? <LockDisplay /> : null}
         </Toolbar>
       </AppBar>
       <Toolbar /> {/* so that app bar doesn't cover top of content */}
